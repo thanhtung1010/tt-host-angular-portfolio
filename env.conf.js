5d276bb294d37702c132feae9bdfb6b915bf452e
tung trinh <tung.trinh@bstarsolutions.com>
2024-04-22 10:32:18 +0700

    TEST ENV

--- env.conf.js
+++ env.conf.js
@@ -5,7 +5,7 @@ const successColor = '\x1b[32m%s\x1b[0m';
 const checkSign = '\u{2705}';
 // const dotenv = require('dotenv').config({path: 'src/.env'}); ;
 
-const envFile = fs.readFileSync('./src/environments/environment.prod.ts', 'utf-8');
+let envFile = fs.readFileSync('./src/environments/environment.prod.ts', 'utf-8');
 const numberField = [
   '_DEFAULT_PAGE_SIZE_',
   '_TIMEOUT_MS_'
@@ -18,9 +18,8 @@ for (let field in envConfig) {
     if (numberField.includes(field)) {
       field = "'" + field + "'";
     }
-    console.log('field:', field);
     console.log('config value:', envConfig[field]);
-    envFile.replace(field, envConfig[field]);
+    envFile = envFile.replace(field, envConfig[field]);
   }
 }
 
